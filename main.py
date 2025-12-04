f019d4825261ae48b52ec800508b74756c2371dd
kdpc614@gmail.com <kdpc614@gmail.com>
2025-12-03 19:43:51 -0500

    feat: se agrego el menú

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from typing import List
 from src.start.welcome import welcomeText
+from src.start.menu_list import MenuList
+
 
 class GestorRiataPlataform:
     def __init__(self):
         self.company_name = ""
         self.id = 1
-        self.menu_list = []
+        self.menu_list: List[MenuList] = []
+        self.menu__list_item: None | MenuList = None
+        self.is_running = False
 
     def on_program(self):
         print(welcomeText)
@@ -13,14 +18,54 @@ class GestorRiataPlataform:
 
         print(f"Empresa: {self.company_name}")
 
+        self.create_menu()
+
+        self.running_menu()
+
     def create_menu(self):
-        self.menu_list = [
-           
-            "",
-            "",
-            "",
-            " --> Productos de tiendas",
-        ]
+        menu = MenuList()
+        self.menu_list = menu.menu_list
+
+    def running_menu(self):
+        if len(self.menu_list):
+            self.is_running = True
+
+            while self.is_running:
+                if self.menu__list_item == None:
+                    for index in range(len(self.menu_list)):
+                        item_menu = self.menu_list[index]
+                        print(f"{item_menu["id"]}. {item_menu["name"]}")
+
+                item_menu_selected = int(
+                    input("Escriba el número al acción que quiera ingresar: ")
+                )
+
+                def item_found():
+                    self.menu__list_item = self.menu_list[item_menu_selected - 1]
+                    print(f"Has seleccionado {self.menu__list_item["name"]}")
+                    return self.menu__list_item
+
+                if item_menu_selected == 1:
+                    item_found()
+                elif item_menu_selected == 2:
+                    item_found()
+                elif item_menu_selected == 3:
+                    item_found()
+                elif item_menu_selected == 4:
+                    item_found()
+                elif item_menu_selected == 5:
+                    item_found()
+                elif item_menu_selected == 6:
+                    is_sure_program = input(
+                        "Está seguro que quiere cerrar el programa: (yes / no) "
+                    )
+                    if is_sure_program == "yes":
+                        self.is_running = False
+                else:
+                    self.is_running = False
+
+        else:
+            print("No se pudo no mostrar el menú")
 
 
 fisrtCompany = GestorRiataPlataform()
--- src/start/menu_list.py
+++ src/start/menu_list.py
@@ -1,20 +1,26 @@
-menu_list = [
-    {"id": 1, "name": "Roles", "sub_menu_list": []},
-    {"id": 2, "name": "Usuarios", "sub_menu_list": []},
-    {
-        "id": 3,
-        "name": "Tiendas Provedoras",
-        "sub_menu_list": [
-            {"id": 1, "name": "Materiales", "sub_menu_list": []},
-        ],
-    },
-    {"id": 4, "name": "Stock Inventario", "sub_menu_list": []},
-    {
-        "id": 5,
-        "name": "Facturación",
-        "sub_menu_list": [
-            {"id": 1, "name": "Facturas de Materiales", "sub_menu_list": []},
-            {"id": 2, "name": "Facturas de Stock Vendido", "sub_menu_list": []},
-        ],
-    },
-]
+class MenuList:
+    def __init__(self):
+        self.id = 0
+        self.name = ''
+        self.sub_menu_list = []
+        self.menu_list = [
+            {"id": 1, "name": "Roles", "sub_menu_list": []},
+            {"id": 2, "name": "Usuarios", "sub_menu_list": []},
+            {
+                "id": 3,
+                "name": "Tiendas Provedoras",
+                "sub_menu_list": [
+                    {"id": 1, "name": "Materiales", "sub_menu_list": []},
+                ],
+            },
+            {"id": 4, "name": "Stock Inventario", "sub_menu_list": []},
+            {
+                "id": 5,
+                "name": "Facturación",
+                "sub_menu_list": [
+                    {"id": 1, "name": "Facturas de Materiales", "sub_menu_list": []},
+                    {"id": 2, "name": "Facturas de Stock Vendido", "sub_menu_list": []},
+                ],
+            },
+            {"id": 6, "name": "Cerrar Programa", "sub_menu_list": []},
+        ]
