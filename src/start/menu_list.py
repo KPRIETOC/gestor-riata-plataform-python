afbc88ae9cd3210e7dab2a2101bb0a7a7653f955
VakanoDanSena <vakanodan@gmail.com>
2025-12-03 20:59:12 -0500

    feat: add new menu

--- src/start/menu_list.py
+++ src/start/menu_list.py
@@ -1,26 +1,19 @@
 class MenuList:
-    def __init__(self):
-        self.id = 0
-        self.name = ''
-        self.sub_menu_list = []
-        self.menu_list = [
-            {"id": 1, "name": "Roles", "sub_menu_list": []},
-            {"id": 2, "name": "Usuarios", "sub_menu_list": []},
-            {
-                "id": 3,
-                "name": "Tiendas Provedoras",
-                "sub_menu_list": [
-                    {"id": 1, "name": "Materiales", "sub_menu_list": []},
-                ],
-            },
-            {"id": 4, "name": "Stock Inventario", "sub_menu_list": []},
-            {
-                "id": 5,
-                "name": "Facturación",
-                "sub_menu_list": [
-                    {"id": 1, "name": "Facturas de Materiales", "sub_menu_list": []},
-                    {"id": 2, "name": "Facturas de Stock Vendido", "sub_menu_list": []},
-                ],
-            },
-            {"id": 6, "name": "Cerrar Programa", "sub_menu_list": []},
-        ]
+    def __init__(self, id: int, name: str, sub_menu_list: list = []):
+        self.id: int = id
+        self.name: str = name
+        self.sub_menu_list: list[MenuList] = sub_menu_list
+
+
+menu_list: list[MenuList] = []
+menu_list_current_id = len(menu_list) + 1
+
+menu_rol = MenuList(menu_list_current_id, "Roles")
+menu_users = MenuList(menu_list_current_id, "Usuarios")
+menu_supplier_stores = MenuList(menu_list_current_id, "Tiendas Provedoras")
+menu_stock = MenuList(menu_list_current_id, "Stock Inventario")
+menu_invoices = MenuList(menu_list_current_id, "Facturas")
+
+menu_list.append(menu_rol, menu_users, menu_supplier_stores, menu_stock, menu_invoices)
+
+print(menu_list)
